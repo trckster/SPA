eeafaf4536090ba55f628dbcb354a21b466a0c08
Daniil <dan-trofimov@mail.ru>
2019-10-07 04:06:36 +0300

    wip

--- SmartParkingApp/ParkingManager.cs
+++ SmartParkingApp/ParkingManager.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ParkingApp
 {
     class ParkingManager
     {
-        private List ActiveParkingSessions = new List<ParkingSession>;
-        private List CompletedParkingSessions = new List<ParkingSession>;
-        private List Tariff = new List<Tariff>;
+        private List ActiveParkingSessions = new List<ParkingSession>();
+        private List CompletedParkingSessions = new List<ParkingSession>();
+        private List Tariff = new List<Tariff>();
 
         private const int ParkingCapacity = 450;
         private int FreeLeavePeriod = 15;
@@ -14,24 +16,25 @@ namespace ParkingApp
         /* BASIC PART */
         public ParkingSession EnterParking(string carPlateNumber)
         {
-            if (this-)
-            /* Check that there is a free parking place (by comparing the parking capacity 
-             * with the number of active parking sessions). If there are no free places, return null
-             * 
-             * Also check that there are no existing active sessions with the same car plate number,
-             * if such session exists, also return null
-             * 
-             * Otherwise:
-             * Create a new Parking session, fill the following properties:
-             * EntryDt = current date time
-             * CarPlateNumber = carPlateNumber (from parameter)
-             * TicketNumber = unused parking ticket number = generate this programmatically
-             * 
-             * Add the newly created session to the list of active sessions
-             * 
-             * Advanced task:
-             * Link the new parking session to an existing user by car plate number (if such user exists)            
-             */
+            if (ParkingManager.ParkingCapacity <= this.ActiveParkingSessions.Length)
+                
+                /* Check that there is a free parking place (by comparing the parking capacity 
+                 * with the number of active parking sessions). If there are no free places, return null
+                 * 
+                 * Also check that there are no existing active sessions with the same car plate number,
+                 * if such session exists, also return null
+                 * 
+                 * Otherwise:
+                 * Create a new Parking session, fill the following properties:
+                 * EntryDt = current date time
+                 * CarPlateNumber = carPlateNumber (from parameter)
+                 * TicketNumber = unused parking ticket number = generate this programmatically
+                 * 
+                 * Add the newly created session to the list of active sessions
+                 * 
+                 * Advanced task:
+                 * Link the new parking session to an existing user by car plate number (if such user exists)            
+                 */
             throw new NotImplementedException();
         }
 
